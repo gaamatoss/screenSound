8b866efcd14b6c3f07f114b63021b9e54bcb6224
Gabriel Matos <gsiva505@hotmail.com>
2023-08-08 20:32:17 -0300

    add: finalizando projeto screensound

---
+++ ScreenSound/Podcast.cs
@@ -0,0 +1,27 @@
+﻿public class Podcast
+{
+	public Podcast(string host, string nome)
+	{
+		Host = host;
+		Nome = nome;
+	}
+	private List<Episodio> episodios = new List<Episodio>();
+	public string Host { get; }
+	public string Nome { get; }
+	public int TotalEpisodios => episodios.Count();
+
+	public void AdicionarEpisodio(Episodio episodio)
+	{
+		episodios.Add(episodio);
+	}
+
+	public void ExibirDetalhes()
+	{
+		Console.WriteLine($"Podcast {Nome}, apresentado por {Host} \n");
+		foreach (Episodio item in episodios)
+		{
+			Console.WriteLine(item.Resumo);
+		}
+		Console.WriteLine($"\n\n Este podcast possui {TotalEpisodios} episódio.");
+	}
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,17 +1,41 @@
-﻿Banda queen = new Banda("Queen");
-Album albumDoQueen = new Album("A night at the opera");
-Musica musica1 = new Musica(queen,"Love of my life");
-musica1.Duracao = 234;
-musica1.Disponivel = true;
-Musica musica2 = new Musica(queen, "Bohemin Rhapsody");
-musica2.Duracao = 334;
-musica2.Disponivel = false;
-albumDoQueen.AdicionarMusica(musica1);
-albumDoQueen.AdicionarMusica(musica2);
-albumDoQueen.ListarAlbum();
-queen.AddToAlbum(albumDoQueen);
-queen.ExibirDiscografia();
-Console.WriteLine("-----------");
-musica1.ExibirFicaTecnica();
-Console.WriteLine("-----------");
-musica2.ExibirFicaTecnica();
+﻿//Banda queen = new Banda("Queen");
+//Album albumDoQueen = new Album("A night at the opera");
+//Musica musica1 = new Musica(queen,"Love of my life");
+//musica1.Duracao = 234;
+//musica1.Disponivel = true;
+//Musica musica2 = new Musica(queen, "Bohemin Rhapsody");
+//musica2.Duracao = 334;
+//musica2.Disponivel = false;
+//albumDoQueen.AdicionarMusica(musica1);
+//albumDoQueen.AdicionarMusica(musica2);
+//albumDoQueen.ListarAlbum();
+//queen.AddToAlbum(albumDoQueen);
+//queen.ExibirDiscografia();
+//Console.WriteLine("-----------");
+//musica1.ExibirFicaTecnica();
+//Console.WriteLine("-----------");
+//musica2.ExibirFicaTecnica();
+
+Episodio ep1 = new(1, 22,"teste");
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
+
+Episodio ep2 = new(2, 22, "teste1");
+ep2.AdicionarConvidados("Maria");
+ep2.AdicionarConvidados("Marcelo");
+
+Episodio ep3 = new(3, 22, "teste2");
+ep3.AdicionarConvidados("Maria");
+ep3.AdicionarConvidados("Marcelo");
+
+Episodio ep4 = new(4, 22, "teste3");
+ep4.AdicionarConvidados("Maria");
+ep4.AdicionarConvidados("Marcelo");
+
+Podcast podcast = new("Gabriel", "PodTuco");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.AdicionarEpisodio(ep3);
+podcast.AdicionarEpisodio(ep4);
+podcast.ExibirDetalhes();
+
